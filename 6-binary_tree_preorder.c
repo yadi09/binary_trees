98f5e5c16a526a84842b33637d05b23b0d46a354
yadi09 <yadamzerterefe09@gmail.com>
2024-04-11 13:02:45 +0100

    ...

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function
+ * @tree: ..........
+ * @func: ..........
+ * Return: ..........
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL)
+return;
+
+(*func)(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+}

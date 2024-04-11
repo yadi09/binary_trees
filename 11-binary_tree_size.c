99d89c4820ffb914e584457aad16be0152a52bcb
yadi09 <yadamzerterefe09@gmail.com>
2024-04-12 00:03:26 +0100

    Task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function
+ * @tree: .......
+ * Return: .......
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
+}

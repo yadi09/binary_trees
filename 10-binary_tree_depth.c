349dab9814f5f0a3db13c1222e6d1bdbf8ac6bb2
yadi09 <yadamzerterefe09@gmail.com>
2024-04-11 23:52:22 +0100

    Task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function
+ * @tree: ..........
+ * Return: .......
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t depth = 0;
+
+
+if (tree == NULL)
+return (0);
+
+while (tree->parent != NULL)
+{
+depth++;
+tree = tree->parent;
+}
+
+return (depth);
+}

4fac031750a73f3e4280157c349d720d65d32bfe
yadi09 <yadamzerterefe09@gmail.com>
2024-04-11 22:20:34 +0100

    Task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - function
+ * @tree: ..........
+ * @func: ...........
+ * Return: ..........
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+return;
+
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+(*func)(tree->n);
+}

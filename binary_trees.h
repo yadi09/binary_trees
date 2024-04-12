eb3e2a368ee0baf81141de81ee48a436e4892830
yadi09 <yadamzerterefe09@gmail.com>
2024-04-12 01:55:25 +0100

    Task 17

--- binary_trees.h
+++ binary_trees.h
@@ -37,6 +37,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 #endif /* _BINARY_TREES_H_ */

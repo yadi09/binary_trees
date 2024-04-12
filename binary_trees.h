fb3b9687a13a2ee018d33003b3cdb8b12cf64b67
yadi09 <yadamzerterefe09@gmail.com>
2024-04-12 01:32:33 +0100

    Modify the header file

--- binary_trees.h
+++ binary_trees.h
@@ -34,7 +34,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */

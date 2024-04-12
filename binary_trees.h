6c636d6371e276bdb1dadc4b61e7d76ec0d3a582
yadi09 <yadamzerterefe09@gmail.com>
2024-04-12 01:28:46 +0100

    Task 7

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif /* _BINARY_TREES_H_ */
